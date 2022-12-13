4fc3c040eb96983d199fe8bac7ced97910f82d8a
Ankeraout <ankeraout@yahoo.com>
2022-12-13 13:09:48 +0100

    Implemented joypad

--- include/core/joypad.h
+++ include/core/joypad.h
@@ -1,10 +1,23 @@
 #ifndef __INCLUDE_CORE_JOYPAD_H__
 #define __INCLUDE_CORE_JOYPAD_H__
 
+#include <stdbool.h>
 #include <stdint.h>
+
+enum te_coreJoypadInput {
+    E_JOYPADINPUT_RIGHT,
+    E_JOYPADINPUT_LEFT,
+    E_JOYPADINPUT_UP,
+    E_JOYPADINPUT_DOWN,
+    E_JOYPADINPUT_A,
+    E_JOYPADINPUT_B,
+    E_JOYPADINPUT_SELECT,
+    E_JOYPADINPUT_START
+};
 
 void coreJoypadReset(void);
 uint8_t coreJoypadRead(uint16_t p_address);
 void coreJoypadWrite(uint16_t p_address, uint8_t p_value);
+void coreJoypadSetInput(enum te_coreJoypadInput p_input, bool p_pressed);
 
 #endif
--- src/core/joypad.c
+++ src/core/joypad.c
@@ -1,15 +1,67 @@
+#include <stdbool.h>
 #include <stdint.h>
 
+#include "common.h"
 #include "core/joypad.h"
 
-void coreJoypadReset(void) {
+static bool s_coreJoypadSelectAction;
+static bool s_coreJoypadSelectDirection;
+static uint8_t s_coreJoypadRegisterJoyp;
+static bool s_coreJoypadButtonState[8];
 
+static void coreJoypadUpdate(void);
+
+void coreJoypadReset(void) {
+    s_coreJoypadRegisterJoyp = 0xff;
+    s_coreJoypadSelectAction = false;
+    s_coreJoypadSelectDirection = false;
 }
 
 uint8_t coreJoypadRead(uint16_t p_address) {
+    M_UNUSED_PARAMETER(p_address);
 
+    return s_coreJoypadRegisterJoyp;
 }
 
 void coreJoypadWrite(uint16_t p_address, uint8_t p_value) {
+    M_UNUSED_PARAMETER(p_address);
 
+    s_coreJoypadSelectAction = (p_value & 0x20) == 0;
+    s_coreJoypadSelectDirection = (p_value & 0x10) == 0;
+
+    coreJoypadUpdate();
+}
+
+void coreJoypadSetInput(enum te_coreJoypadInput p_input, bool p_pressed) {
+    s_coreJoypadButtonState[p_input] = p_pressed;
+
+    coreJoypadUpdate();
+}
+
+static void coreJoypadUpdate(void) {
+    s_coreJoypadRegisterJoyp = 0xff;
+
+    if(s_coreJoypadSelectAction) {
+        s_coreJoypadRegisterJoyp &= 0xdf;
+
+        int l_shift = 0;
+
+        for(int l_button = 4; l_button < 8; l_button++) {
+            if(s_coreJoypadButtonState[l_button]) {
+                s_coreJoypadRegisterJoyp &= ~(1 << l_shift);
+            }
+
+            l_shift++;
+        }
+    }
+
+    if(s_coreJoypadSelectDirection) {
+        s_coreJoypadRegisterJoyp &= 0xef;
+
+        for(int l_button = 0; l_button < 4; l_button++) {
+            if(s_coreJoypadButtonState[l_button]) {
+                s_coreJoypadRegisterJoyp &= ~(1 << l_button);
+            }
+        }
+    }
 }
